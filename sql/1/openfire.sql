d16b3bffe12b8c8422b7f383f447d7b435a2fb40
Dan Caseley <dan@caseley.me.uk>
2020-10-05 16:08:16 +0100

    Enable FMUC in ofproperties

--- sql/1/openfire.sql
+++ sql/1/openfire.sql
@@ -723,6 +723,7 @@ provider.vcard.className	org.jivesoftware.openfire.vcard.DefaultVCardProvider	0	
 provider.securityAudit.className	org.jivesoftware.openfire.security.DefaultSecurityAuditProvider	0	\N
 provider.user.className	org.jivesoftware.openfire.user.DefaultUserProvider	0	\N
 update.lastCheck	1590672970635	0	\N
+xmpp.muc.room.fmuc.enabled	true	0	\N
 \.
 
 
