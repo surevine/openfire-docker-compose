5e03c085f94230e149b65ab1bf2d1903566397b2
Dan Caseley <dan@caseley.me.uk>
2021-01-12 13:02:50 +0000

    Add support for specifying openfire tags via env or parameter
    Setting .env to "461" or running `./start.sh 461` will get openfire:461 rather than latest

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ COMPOSE_FILE=docker-compose-federated.yml
 if [ "$1" = "-c" ]; then
   echo "Starting a clustered environment."
   COMPOSE_FILE=docker-compose-clustered.yml
+  shift
 else
   echo "Starting a federated environment (use -c to start a clustered environment instead)."
 fi
@@ -17,4 +18,8 @@ rm -rf _data
 mkdir _data
 cp -r xmpp _data/
 
+if [ -n "$1" ]; then
+  echo "Using Openfire tag: $1"
+  export OPENFIRE_TAG=$1
+fi
 docker-compose -f $COMPOSE_FILE up
