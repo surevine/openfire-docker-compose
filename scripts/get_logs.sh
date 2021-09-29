0f51ad721352c181dd49ce31fbffb9325d86026d
Emiel van der Herberg <evdherberg@gmail.com>
2021-09-29 15:15:29 +0200

    Updated cert for otherdomain (#38)
    New self signed certs for the other domain.
    Pull in all log files with get_logs.
    
    Co-authored-by: Emiel van der Herberg <emiel.van.der.herberg@marviq.com>

--- scripts/get_logs.sh
+++ scripts/get_logs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-docker exec -t openfire-docker-compose_xmpp1_1 cat /usr/local/openfire/logs/all.log > 1all.log
-docker exec -t openfire-docker-compose_xmpp2_1 cat /usr/local/openfire/logs/all.log > 2all.log
-docker exec -t openfire-docker-compose_xmpp2_1 cat /usr/local/openfire/logs/jive.audit-20210202-000.log > jiveaudit.log
+docker exec -t openfire-docker-compose_xmpp1_1 cat /usr/local/openfire/logs/all.log > 1-all.log
+docker exec -t openfire-docker-compose_xmpp2_1 cat /usr/local/openfire/logs/all.log > 2-all.log
+docker exec -t openfire-docker-compose_xmpp3_1 cat /usr/local/openfire/logs/all.log > 3-all.log
+docker exec -t openfire-docker-compose_xmpp2_1 cat /usr/local/openfire/logs/jive.audit-20210202-000.log > 2-jiveaudit.log
+docker exec -t openfire-docker-compose_otherxmpp_1 cat /usr/local/openfire/logs/all.log > other-all.log
 
 #docker cp openfire-docker-compose_xmpp1_1:/usr/local/openfire/logs/all.log 1all.log
 #docker cp openfire-docker-compose_xmpp2_1:/usr/local/openfire/logs/all.log 2all.log
