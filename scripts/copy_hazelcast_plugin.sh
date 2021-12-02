d6f5761fbc6eb85251e28f6a31b2c56c2b5cb7de
Emiel van der Herberg <emiel.van.der.herberg@marviq.com>
2021-12-02 13:06:14 +0100

    No copying of hazelcast plugin to non-clustered openfire environments. No default inclusion of monitoring plugin.

--- scripts/copy_hazelcast_plugin.sh
+++ scripts/copy_hazelcast_plugin.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # For when you've built the hazelcast plugin locally and want this in your next stack
 cp ../openfire-hazelcast-plugin/target/hazelcast-openfire-plugin-assembly.jar ./plugins_for_clustered/hazelcast.jar
-cp ../openfire-hazelcast-plugin/target/hazelcast-openfire-plugin-assembly.jar ./plugins_for_federated/hazelcast.jar
-cp ../openfire-hazelcast-plugin/target/hazelcast-openfire-plugin-assembly.jar ./plugins_for_other/hazelcast.jar
