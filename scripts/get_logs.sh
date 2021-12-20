c7efe32e96aba5271e3095b78e03082bfdf64994
Dan Caseley <dan@caseley.me.uk>
2021-12-20 11:28:33 +0000

    Update scripts to ignore fixed name formats

--- scripts/get_logs.sh
+++ scripts/get_logs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-docker exec -t openfire-docker-compose-xmpp1-1 cat /usr/local/openfire/logs/openfire.log > 1-openfire.log
-docker exec -t openfire-docker-compose-xmpp2-1 cat /usr/local/openfire/logs/openfire.log > 2-openfire.log
-docker exec -t openfire-docker-compose-xmpp3-1 cat /usr/local/openfire/logs/openfire.log > 3-openfire.log
-docker exec -t openfire-docker-compose_otherxmpp_1 cat /usr/local/openfire/logs/openfire.log > other-openfire.log
+CONTAINER_ONE=$(docker ps --filter status=running --format "{{.Names}}" | grep -E openfire-docker.+xmpp1.1)
+CONTAINER_TWO=$(docker ps --filter status=running --format "{{.Names}}" | grep -E openfire-docker.+xmpp2.1)
+CONTAINER_THREE=$(docker ps --filter status=running --format "{{.Names}}" | grep -E openfire-docker.+xmpp3.1)
+CONTAINER_OTHER=$(docker ps --filter status=running --format "{{.Names}}" | grep -E openfire-docker.+otherxmpp.1)
+
+[ -n "$CONTAINER_ONE" ] && docker exec -t "$CONTAINER_ONE" cat /usr/local/openfire/logs/openfire.log > 1-openfire.log
+[ -n "$CONTAINER_TWO" ] && docker exec -t "$CONTAINER_TWO" cat /usr/local/openfire/logs/openfire.log > 2-openfire.log
+[ -n "$CONTAINER_THREE" ] && docker exec -t "$CONTAINER_THREE" cat /usr/local/openfire/logs/openfire.log > 3-openfire.log
+[ -n "$CONTAINER_OTHER" ] && docker exec -t "$CONTAINER_OTHER" cat /usr/local/openfire/logs/openfire.log > other-openfire.log
--- scripts/peek_at_database.sh
+++ scripts/peek_at_database.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Get a dump of the Openfire database in db_1 to peek at
-docker exec -t openfire-docker-compose_db_1 pg_dump -U openfire openfire > peek.sql
+CONTAINER_NAME=$(docker ps --filter status=running --format "{{.Names}}" | grep -E openfire-docker.+db.1)
+docker exec -t "$CONTAINER_NAME" pg_dump -U openfire openfire > peek.sql
