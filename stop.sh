097d29d8f57c405b4a1134adbcc812b6a9494eef
Dan Caseley <dan@caseley.me.uk>
2022-03-11 23:13:35 +0000

    Update scripts. More reliable stop, and mitigate #18

--- stop.sh
+++ stop.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# A list of all possible docker compose files that might've been used to start the stack
-
-docker-compose \
- -f docker-compose-clustered.yml \
- -f docker-compose-federated.yml \
- -f docker-compose-otherdomain.yml \
- -f docker-compose-logging.yml \
-down
+docker-compose -p openfire down
