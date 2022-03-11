bd38d3e9e6f5ff5985f42a4a69bbecffe78e1723
Dan Caseley <dan@caseley.me.uk>
2022-03-11 23:18:08 +0000

    Update scripts to account for file movement

--- scripts/copy_hazelcast_plugin.sh
+++ scripts/copy_hazelcast_plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # For when you've built the hazelcast plugin locally and want this in your next stack
-cp ../openfire-hazelcast-plugin/target/hazelcast-openfire-plugin-assembly.jar ./plugins_for_clustered/hazelcast.jar
+cp ../openfire-hazelcast-plugin/target/hazelcast-openfire-plugin-assembly.jar ./cluster/plugins/hazelcast.jar
--- scripts/copy_monitoring_plugin.sh
+++ scripts/copy_monitoring_plugin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # For when you've built the monitoring plugin locally and want this in your next stack
-cp ../openfire-monitoring-plugin/target/monitoring-openfire-plugin-assembly.jar ./plugins_for_clustered/monitoring.jar
-cp ../openfire-monitoring-plugin/target/monitoring-openfire-plugin-assembly.jar ./plugins_for_federated/monitoring.jar
-cp ../openfire-monitoring-plugin/target/monitoring-openfire-plugin-assembly.jar ./plugins_for_other/monitoring.jar
+cp ../openfire-monitoring-plugin/target/monitoring-openfire-plugin-assembly.jar ./cluster/plugins/monitoring.jar
+cp ../openfire-monitoring-plugin/target/monitoring-openfire-plugin-assembly.jar ./cluster_with_federation/plugins/monitoring.jar
+cp ../openfire-monitoring-plugin/target/monitoring-openfire-plugin-assembly.jar ./cluster_with_federation/plugins_for_other/monitoring.jar
+cp ../openfire-monitoring-plugin/target/monitoring-openfire-plugin-assembly.jar ./federation/plugins/monitoring.jar
