eea5e4bce6fb342c9e12c1179f1bc910458cc456
Dan Caseley <dan@caseley.me.uk>
2023-09-10 20:57:51 +0100

    Add system property defaults

--- clamav/sql/openfire.sql
+++ clamav/sql/openfire.sql
@@ -723,7 +723,10 @@ provider.securityAudit.className	org.jivesoftware.openfire.security.DefaultSecur
 provider.user.className	org.jivesoftware.openfire.user.DefaultUserProvider	0	\N
 passwordKey	YJ1nKWyrMeGvTKu	0	\N
 update.lastCheck	1605956807055	0	\N
-
+plugin.httpfileupload.clamavEnabled	true	0	\N
+plugin.httpfileupload.clamavHost	clamav	0	\N
+plugin.httpfileupload.announcedWebProtocol	http	0	\N
+plugin.httpfileupload.announcedWebPort	7070	0	\N
 \.
 
 
